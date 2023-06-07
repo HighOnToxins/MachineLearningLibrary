e97308faea9412a4bedfa7a5f5161793eea6f6ff
Toxic <markusbolsen@gmail.com>
2023-06-07 16:27:03 +0200

    Finished defivative invoke method for Convolution2DAgent.

--- MachineLearningLibrary/Convolution2DAgent.cs
+++ MachineLearningLibrary/Convolution2DAgent.cs
@@ -1,6 +1,7 @@
 ﻿
 namespace MachineLearningLibrary;
 
+//TODO: IDEA: Create a one-axis-kernal convolution.
 public sealed class Convolution2DAgent: IAgent
 {
 
@@ -10,6 +11,10 @@ public sealed class Convolution2DAgent: IAgent
 
     private readonly int offsetX;
     private readonly int offsetY;
+
+    private readonly int inputCount;
+
+    //TODO: Consider adding a bias.
 
     public Convolution2DAgent(float[][] kernal, int inputWidth, int inputHeight, int outputWidth, int outputHeight)
     {
@@ -29,6 +34,7 @@ public sealed class Convolution2DAgent: IAgent
         }
 
         variableCount = kernal.Length * kernal0length;
+        inputCount = OutputWidth * OutputHeight;
 
         offsetX = (InputWidth - OutputWidth - KernalWidth) / 2;
         offsetY = (InputHeight - OutputHeight - KernalHeight) / 2;
@@ -57,6 +63,8 @@ public sealed class Convolution2DAgent: IAgent
 
     public void Invoke(in IReadOnlyList<float> value, out IReadOnlyList<float> result)
     {
+        if(value.Count != OutputHeight * OutputWidth) throw new NotImplementedException();
+
         float[] valueResult = new float[OutputWidth * OutputHeight];
 
         for(int outX = 0; outX < OutputWidth; outX++)
@@ -64,7 +72,7 @@ public sealed class Convolution2DAgent: IAgent
             for(int outY = 0; outY < OutputHeight; outY++)
             {
                 float average = 0;
-                int countPlusOne = 1;
+                int count = 0;
 
                 int outOffsetX = offsetX + outX;
                 int outoffsetY = offsetY + outY;
@@ -82,21 +90,80 @@ public sealed class Convolution2DAgent: IAgent
                         }
 
                         int valI = valX * InputHeight + valY;
-                        float v = kernal[kerX][kerY] * value[valI];
-                        average += (v - average) / countPlusOne;
+                        average += kernal[kerX][kerY] * value[valI];
+                        count++;
                     }
                 }
 
-                valueResult[outX * OutputHeight + outY] = average;
+                valueResult[outX * OutputHeight + outY] = Math.Max(0, average / count);
             }
         }
 
         result = valueResult;
     }
 
-    public void Invoke(in IReadOnlyList<float> value, in IReadOnlyList<float>? gradient, out IReadOnlyList<float> valueResult, out IReadOnlyList<float> derivativeResult, int varIndex = -1)
+    public void Invoke(
+        in IReadOnlyList<float> value,
+        in IReadOnlyList<float>? gradient, 
+        out IReadOnlyList<float> valueOutput, 
+        out IReadOnlyList<float> derivativeResult, 
+        int varIndex = -1)
     {
-        throw new NotImplementedException();
+        Invoke(value, out IReadOnlyList<float> valueResult);
+
+        float[] derivative = new float[OutputWidth * OutputHeight];
+
+        if(gradient is not null)
+        {
+            if(gradient.Count != InputWidth * InputHeight) throw new ArgumentException();
+
+            for(int outX = 0; outX < OutputWidth; outX++)
+            {
+                for(int outY = 0; outY < OutputHeight; outY++)
+                {
+                    int outI = outX * OutputHeight + outY;
+                    if(valueResult[outI] == 0) continue;
+
+                    float average = 0;
+                    int count = 0;
+
+                    int outOffsetX = offsetX + outX;
+                    int outoffsetY = offsetY + outY; //TODO: Consider incrementing these values rather that computing them.
+
+                    for(int kerX = 0; kerX < KernalWidth; kerX++)
+                    {
+                        for(int kerY = 0; kerY < KernalHeight; kerY++)
+                        {
+                            int valX = outOffsetX + kerX;
+                            int valY = outoffsetY + kerY;
+                            int valI = valX * InputHeight + valY;
+
+                            if(0 <= valI && valI < inputCount)
+                            {
+                                average += kernal[kerX][kerY] * gradient[valI];
+                                count++;
+                            }
+                        }
+                    }
+
+                    derivative[outI] = average / count;
+
+                    int kerDifX = varIndex / KernalWidth;
+                    int kerDifY = varIndex % KernalWidth;
+                    int valDifX = outOffsetX + kerDifX;
+                    int valDifY = outoffsetY + kerDifY;
+                    int valDifI = valDifX * InputHeight + valDifY;
+
+                    if(0 <= valDifI && valDifI < inputCount)
+                    {
+                        derivative[outI] += value[valDifI];
+                    }
+                }
+            }
+        }
+
+        valueOutput = valueResult;
+        derivativeResult = derivative;
     }
 
     public void WriteToFile(BinaryWriter binWriter)
